67a0226d120294775707d33a48a27d4b29223a75
Ben Marks <bemarks@ebay.com>
2015-10-25 13:33:42 +0100

    Routing, ACL, and i18n

---
+++ Controller/Adminhtml/Bookmark/Index.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace MagentoHackathon\GridConfigShare\Controller\Adminhtml\Bookmark;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('MagentoHackathon_GridConfigShare::bookmarks');
+    }
+
+    /**
+     * Displays a list of Grid View settings
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->addBreadcrumb(__('Manage Grid Views'), __('Manage Grid Views'));
+
+        return $resultPage;
+    }
+}
